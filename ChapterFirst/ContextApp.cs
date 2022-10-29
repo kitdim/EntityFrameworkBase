149daefa7614f5ee2d48c2fa097b3ca97da82425
kitdim <kitaykin9808@gmail.com>
2022-10-29 22:12:56 +0300

    Класс контекста

---
+++ ChapterFirst/ContextApp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChapterFirst
+{
+    class ContextApp : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+
+        public ContextApp()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB;Database = TestDb;Trusted_Connection = true");
+        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Ignore<Company>();
+        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Table>();
+            //modelBuilder.Ignore<Company>();
+        }
+    }
+}
