04b5ec71d73d88405f92fc94608018470c407c0f
kitdim <kitaykin9808@gmail.com>
2022-11-05 21:32:02 +0300

    Обновил класс контекста

--- ChapterFirst/ContextApp.cs
+++ ChapterFirst/ContextApp.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace ChapterFirst
 {
     class ContextApp : DbContext
     {
         public DbSet<Phone> Phones { get; set; }
+        public DbSet<Company> Сompanies { get; set; }
 
         public ContextApp()
         {
@@ -15,15 +15,6 @@ namespace ChapterFirst
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB;Database = TestDb;Trusted_Connection = true");
-        }
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Ignore<Company>();
-        //}
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Table>();
-            //modelBuilder.Ignore<Company>();
         }
     }
 }
