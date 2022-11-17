8932439d3b59700d952de28c7030c9c9a7ef2fa6
kitdim <kitaykin9808@gmail.com>
2022-11-17 10:21:00 +0300

    Добавил через конслось представление "сначало данные"

---
+++ ChapterFirst_DatabaseFirst/Phone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChapterFirst_DatabaseFirst
+{
+    public partial class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Price { get; set; }
+    }
+}
---
+++ ChapterFirst_DatabaseFirst/Сompany.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChapterFirst_DatabaseFirst
+{
+    public partial class Сompany
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
